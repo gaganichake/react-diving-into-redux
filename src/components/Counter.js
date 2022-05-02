16ee71226dc26fd73da803f76f2b92dca6293dba
Gagan <gaganichake@yahoo.com>
2022-05-02 09:03:31 -0500

    Exporting constants

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
+import { INCREMENT } from '../store/index';
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -7,7 +8,7 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch({ type: INCREMENT});
   };
 
   const increaseHandler = () => {
